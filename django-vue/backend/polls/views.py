50dbf8a2d95878aa2cf797d951e63e6284f6d245
Kunihiro-ikd <kunikuni6129.sddj@gmail.com>
2023-01-22 03:26:51 +0900

    app_name = polls にして動いた

--- django-vue/backend/polls/views.py
+++ django-vue/backend/polls/views.py
@@ -11,16 +11,25 @@ from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # ここに原因がありそう
+    # template = loader.get_template('polls/index.html')
+    template = loader.get_template('index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
