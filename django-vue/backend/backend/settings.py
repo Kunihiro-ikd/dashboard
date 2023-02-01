1b177ae74635a918b273c1354876fa28b9d8a5d6
Kunihiro-ikd <kunikuni6129.sddj@gmail.com>
2023-02-01 18:59:49 +0900

    デプロイ用の設定

--- django-vue/backend/backend/settings.py
+++ django-vue/backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7r328!ia9v6upr2-elqau03gk69xs@j&wi@6!u&*u7bkmh-gt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
 
 
 # Application definition
